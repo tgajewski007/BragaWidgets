3369f4b2ea40f12a7a863545c836ac478aa74f99
Tomasz Gajewski (JamesBlond) <9467981+tgajewski007@users.noreply.github.com>
2017-03-18 16:50:10 +0000

    ASSIGNED - bug 913: Scoring matematyczny https://bugs.krystadvps.nazwa.pl/show_bug.cgi?id=913

---
+++ tests/jquery/funcTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created on 18.03.2017 17:42:24
+ * author Tomasz Gajewski
+ * package frontoffice
+ * error prefix
+ */
+class funcTest extends PHPUnit_Framework_TestCase
+{
+	// -------------------------------------------------------------------------
+	function testIcon()
+	{
+		$retval = faIcon("fa-bath");
+		$this->assertEquals("<i class='fa fa-bath' style=''></i>");
+	}
+	// -------------------------------------------------------------------------
+}
