4e4b16855c408a399bb8cb8fd4d5ec122a33a6c9
tgajewski <tgajewski@4b3bcb9f-7275-4a0f-ade3-bef6ecec5d3a>
2017-06-18 16:54:36 +0000

    +Bootstrap panel

---
+++ widgets/trunk/src/bootstrap/BS.php
@@ -0,0 +1,24 @@
+<?php
+namespace braga\widgets\bootstrap;
+use braga\tools\html\BaseTags;
+
+/**
+ * Created on 18.06.2017 18:50:35
+ * author Tomasz Gajewski
+ * package frontoffice
+ * error prefix
+ */
+class BS
+{
+	// -------------------------------------------------------------------------
+	public static function panel($title, $content)
+	{
+		$retval = "";
+		$head = BaseTags::h3($title, "class='panel-title'");
+		$retval = BaseTags::div($head, "class='panel-heading'");
+		$retval .= BaseTags::div($content, "class='panel-body'");
+		$retval = BaseTags::div($retval, "class='panel panel-default'");
+		return $retval;
+	}
+	// -------------------------------------------------------------------------
+}
