e0ca45c242a3db85c9faf4a028dc4801e527c93a
Tomasz Gajewski (JamesBlond) <9467981+tgajewski007@users.noreply.github.com>
2017-07-04 21:34:49 +0000

    ASSIGNED - bug 1037: Rozszerzenie formatki Leada https://bugs.openfly.eu/show_bug.cgi?id=1037

--- tests/bootstrap/BTDropDownListTest.php
+++ tests/bootstrap/BTDropDownListTest.php
@@ -21,7 +21,7 @@ class BTDropDownListTest extends PHPUnit_Framework_TestCase
 		$actual = $f->out();
 
 		$expected = BaseTags::option("StrangeName", "value='StrangeValue'");
-		$expected = BaseTags::select($expected, "class='form-control dropup show-tick'");
+		$expected = BaseTags::select($expected, "class='form-control show-tick'");
 		$expected = BaseTags::div($expected, "class='form-group'");
 		$expected .= BaseTags::script("initDropDownList($(\"#\"));");
 
@@ -37,7 +37,7 @@ class BTDropDownListTest extends PHPUnit_Framework_TestCase
 
 		$expected = BaseTags::option("StrageName", "value='StrangeValue'");
 		$expected = BaseTags::optgroup($expected, "label='StrageGroup'");
-		$expected = BaseTags::select($expected, "class='form-control dropup show-tick'");
+		$expected = BaseTags::select($expected, "class='form-control show-tick'");
 		$expected = BaseTags::div($expected, "class='form-group'");
 		$expected .= BaseTags::script("initDropDownList($(\"#\"));");
 
@@ -54,7 +54,7 @@ class BTDropDownListTest extends PHPUnit_Framework_TestCase
 
 		$expected = BaseTags::option("StrageName", "value='StrangeValue'");
 		$expected = BaseTags::optgroup($expected, "label='StrageGroup'");
-		$expected = BaseTags::select($expected, "title='-=Wybierz=-' required='true' class='form-control dropup show-tick'");
+		$expected = BaseTags::select($expected, "title='-=Wybierz=-' required='true' class='form-control show-tick'");
 		$expected = BaseTags::div($expected, "class='form-group has-error'");
 		$expected .= BaseTags::script("initDropDownList($(\"#\"));");
 
@@ -72,7 +72,7 @@ class BTDropDownListTest extends PHPUnit_Framework_TestCase
 
 		$expected = BaseTags::option("StrageName", "value='StrangeValue' selected='selected'");
 		$expected = BaseTags::optgroup($expected, "label='StrageGroup'");
-		$expected = BaseTags::select($expected, "class='form-control dropup show-tick'");
+		$expected = BaseTags::select($expected, "class='form-control show-tick'");
 		$expected = BaseTags::div($expected, "class='form-group'");
 		$expected .= BaseTags::script("initDropDownList($(\"#\"));");
 
