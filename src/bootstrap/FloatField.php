ccebcc735fba2fb972d4a2349dfdd944bd391b29
Tomasz Gajewski <tgajewski007@gmail.com>
2024-12-03 18:56:28 +0100

    fix

--- src/bootstrap/FloatField.php
+++ src/bootstrap/FloatField.php
@@ -34,8 +34,8 @@ class FloatField extends \braga\widgets\base\FloatField
 		$this->addAttrib("placeholder", $this->waterMark);
 		$this->addAttrib("data-error", $this->getMessageWhenValidateFail());
 		$pattern = $this->precision > 0
-			? "[+-]?\\d+(\\.\\d{0," . $this->precision . "})?"
-			: "[+-]?\\d+";
+			? "[+-]?\d+(\.\d{0," . $this->precision . "})?"
+			: "[+-]?\d+";
 		$this->addAttrib("pattern", $pattern);
 		$this->setOnKeyUp("\$(this).val(\$(this).val().replace(\",\",\".\"));" . $this->onKeyUp);
 		if($this->required)
