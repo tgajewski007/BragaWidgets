cb1b904b00468fcb63e846501450c1c6fd544549
Tomasz Gajewski <tgajewski007@gmail.com>
2025-10-20 19:12:10 +0200

    Accordion on Bootstrap 5

--- src/bootstrap/Accordion.php
+++ src/bootstrap/Accordion.php
@@ -13,56 +13,44 @@ class Accordion extends HtmlComponent
 {
 	// -------------------------------------------------------------------------
 	/**
-	 * @var array
+	 * @var AccordionItem[]
 	 */
-	protected $items = array();
-	protected $openIdItem;
+	protected array $items = [];
+	// -------------------------------------------------------------------------
+	public function __construct(protected ?string $id = null)
+	{
+		if(empty($id))
+		{
+			$this->id = "A" . getRandomString(8);
+		}
+	}
 	// -------------------------------------------------------------------------
 	public function setActiveItem($id)
 	{
-		$this->openIdItem = $id;
+		if(isset($this->items[$id]))
+		{
+			$this->items[$id]->setActive(true);
+		}
 	}
 	// -------------------------------------------------------------------------
 	public function addItem(AccordionItem $item, $active = false)
 	{
+		$item->setActive($active);
+		$item->setIdAccordion($this->id);
 		$this->items[$item->getId()] = $item;
-		if($active)
-		{
-			$this->openIdItem = $item->getId();
-		}
 	}
 	// -------------------------------------------------------------------------
 	public function out()
 	{
-		$idAccordion = "A" . getRandomString(8);
-		$retval = "";
-		$script = "";
-		foreach($this->items as $item)/* @var $item AccordionItem */
+		foreach($this->items as $item)
 		{
-			$content = BaseTags::div($item->getContent(), "class='panel-body'");
-			$content = BaseTags::div($content, "id='" . $item->getId() . "' class='panel-collapse collapse " . ($item->getId() == $this->openIdItem ? "in" : "") . "'");
-
-			$heading = BaseTags::a($item->getColapseTitle(), "data-toggle='collapse' data-parent='#" . $idAccordion . "' href='#" . $item->getId() . "'");
-			$heading .= $item->getActiveTitle();
-			$heading = BaseTags::h4($heading, 'class="panel-title"');
-			$heading = BaseTags::div($heading, 'class="panel-heading"');
-
-			$rndId = getRandomStringLetterOnly(8);
-			$retval .= BaseTags::div($heading . $content, 'class="panel panel-default" id="' . $rndId . '"');
-			if(!empty($item->getOnShowJavaScript()))
-			{
-				$script .= "\$(\"#" . $rndId . "\").on(\"show.bs.collapse\", function() {" . $item->getOnShowJavaScript() . "});";
-				if($item->getId() == $this->openIdItem)
-				{
-					$script .= $item->getOnShowJavaScript();
-				}
-			}
-			if(!empty($item->getOnLoadJavaScript()))
-			{
-				$script .= $item->getOnLoadJavaScript();
-			}
+			$this->content .= $item->out();
 		}
-		return BaseTags::div($retval, "id='" . $idAccordion . "' class='panel-group' ") . (empty($script) ? "" : BaseTags::script($script));
+		return <<<HTML
+			<div class="accordion" id="{$this->id}">
+				{$this->content}
+			</div>
+			HTML;
 	}
 	// -------------------------------------------------------------------------
 }
--- src/bootstrap/AccordionItem.php
+++ src/bootstrap/AccordionItem.php
@@ -1,37 +1,51 @@
 <?php
 namespace braga\widgets\bootstrap;
+use braga\tools\html\HtmlComponent;
 /**
  * Created on 03.07.2017 21:52:00
  * @author Tomasz Gajewski
  * package frontoffice
  * error prefix
  */
-class AccordionItem
+class AccordionItem extends HtmlComponent
 {
-	// -------------------------------------------------------------------------
-	protected $colapseTitle;
-	protected $activeTitle;
-	protected $content;
-	protected $id;
-	protected $onShowJavaScript;
-	protected $onLoadJavaScript;
-	// ------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------
+	public function __construct(protected ?string $id = null, protected ?string $title = null)
+	{
+	}
+	// -----------------------------------------------------------------------------------------------------------------
+	protected ?string $onShowJavaScript = null;
+	protected ?string $onLoadJavaScript = null;
+	// -----------------------------------------------------------------------------------------------------------------
+	protected ?string $idAccordion = null;
+	public function setIdAccordion(string $idAccordion): void
+	{
+		$this->idAccordion = $idAccordion;
+	}
+	// -----------------------------------------------------------------------------------------------------------------
+	public function setActive(bool $active): void
+	{
+		$this->active = $active;
+	}
+	// -----------------------------------------------------------------------------------------------------------------
+	protected bool $active = false;
+	// -----------------------------------------------------------------------------------------------------------------
 	/**
 	 * @return mixed
 	 */
-	public function getOnShowJavaScript()
+	protected function getOnShowJavaScript()
 	{
 		return $this->onShowJavaScript;
 	}
-	// ------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------
 	/**
 	 * @return mixed
 	 */
-	public function getOnLoadJavaScript()
+	protected function getOnLoadJavaScript()
 	{
 		return $this->onLoadJavaScript;
 	}
-	// ------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------
 	/**
 	 * @param mixed $onShowJavaScript
 	 */
@@ -39,7 +53,7 @@ class AccordionItem
 	{
 		$this->onShowJavaScript = $onShowJavaScript;
 	}
-	// ------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------
 	/**
 	 * @param mixed $onLoad
 	 */
@@ -47,53 +61,88 @@ class AccordionItem
 	{
 		$this->onLoadJavaScript = $onLoad;
 	}
-	// ------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------
 	/**
-	 * @return mixed
+	 * @param $colapseTitle
+	 * @return void
+	 * @deprecated use setTitle
 	 */
-	public function getUrlDynamicContent()
-	{
-		return $this->onLoadJavaScript;
-	}
-	// -------------------------------------------------------------------------
-	public function getColapseTitle()
-	{
-		return $this->colapseTitle;
-	}
-	// -------------------------------------------------------------------------
 	public function setColapseTitle($colapseTitle)
 	{
-		$this->colapseTitle = $colapseTitle;
+		if(empty($this->title))
+		{
+			$this->title = $colapseTitle;
+		}
+		else
+		{
+			$this->title .= $colapseTitle;
+		}
 	}
-	// -------------------------------------------------------------------------
-	public function getActiveTitle()
+	// -----------------------------------------------------------------------------------------------------------------
+	protected function getTitle()
 	{
-		return $this->activeTitle;
+		return $this->title;
 	}
-	// -------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------
+	public function setTitle($title)
+	{
+		$this->title = $title;
+	}
+	// -----------------------------------------------------------------------------------------------------------------
+	/**
+	 * @param $activeTitle
+	 * @return void
+	 * @deprecated use setTitle
+	 */
 	public function setActiveTitle($activeTitle)
 	{
-		$this->activeTitle = $activeTitle;
+		if(empty($this->title))
+		{
+			$this->title = $activeTitle;
+		}
+		else
+		{
+			$this->title .= $activeTitle;
+		}
 	}
-	// -------------------------------------------------------------------------
-	public function getContent()
+	// -----------------------------------------------------------------------------------------------------------------
+	protected function getContent()
 	{
 		return $this->content;
 	}
-	// -------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------
 	public function setContent($content)
 	{
 		$this->content = $content;
 	}
-	// -------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------
 	public function getId()
 	{
 		return $this->id;
 	}
-	// -------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------
 	public function setId($id)
 	{
 		$this->id = $id;
 	}
-	// -------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------
+	public function out()
+	{
+		$isShow = $this->active ? "show" : "";
+		return <<<HTML
+			<div class="accordion-item">
+				<h2 class="accordion-header">
+					<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#{$this->getId()}" aria-expanded="true" aria-controls="{$this->getId()}">
+						{$this->getTitle()}
+					</button>
+				</h2>
+				<div id="{$this->getId()}" class="accordion-collapse collapse {$isShow}" data-bs-parent="#{$this->idAccordion}">
+					<div class="accordion-body">
+						{$this->getContent()}
+					</div>
+				</div>
+			</div>
+			HTML;
+	}
+	// -----------------------------------------------------------------------------------------------------------------
 }
