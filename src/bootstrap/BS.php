f43fd3ce6b67e9c0c0fd18f96192eefcf75bc296
Tomasz Gajewski <tgajewski007@gmail.com>
2025-10-20 21:10:28 +0200

    Accordion on Bootstrap 5

--- src/bootstrap/BS.php
+++ src/bootstrap/BS.php
@@ -13,7 +13,9 @@ class BS
 	{
 		$retval = <<<HTML
 			<div class="{$baseClass}">
-				<div class="card-header"><h3 class="card-title">{$title}</h3></div>
+				<div class="card-header">
+					<h5 class="card-title">{$title}</h5>
+				</div>
 				<div class="card-body">
 					{$content}
 				</div>
