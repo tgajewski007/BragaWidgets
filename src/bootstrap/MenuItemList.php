7713646d9952f6f840e91052df6f619320386ad5
Tomasz Gajewski <tgajewski007@gmail.com>
2025-11-09 18:03:15 +0100

    Accordion on Bootstrap 5

---
+++ src/bootstrap/MenuItemList.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace braga\widgets\bootstrap;
+
+use braga\tools\html\HtmlComponent;
+/**
+ * Created 9.11.2025 17:58
+ * error prefix
+ * @autor Tomasz Gajewski
+ */
+class MenuItemList extends HtmlComponent
+{
+	public function out()
+	{
+		// TODO: Implement out() method.
+	}
+}
